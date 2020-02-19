1e0c7975cdbd295b45a981bbcee68c3756a147cc
Ryan <ryansherring@gmail.com>
2020-02-19 15:45:11 -0800

    adds signup, login, logout, but is still broken

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm, LoginForm
+from django.contrib import auth
+from .models import User
+
+
+
+#signup
+def signup(request):
+  # if post
+  if request.method == "POST":
+    # build out data from form
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    username_form = request.POST['username']
+    email_form = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+  # validate that passwords match
+    if password == password2:
+      # check if username exists in db
+      if User.objects.filter(username=username_form).exists():
+        context = {'error': 'Username is already taken.'}
+        return render(request, 'signup1.html', context)
+      else:
+        if User.objects.filter(email=email_form).exists():
+          context = {'error':'That email already exists.'}
+          return render(request, 'signup1.html', context)
+        else: 
+          # if everything is ok create account
+          user = User.objects.create_user(
+            username=username_form, 
+            email=email_form, 
+            password=password, 
+            first_name=first_name, 
+            last_name=last_name)
+          user.save()
+          return redirect('artist_list')
+    else:
+      context = {'error':'Passwords do not match'}
+      return render(request, 'signup1.html', context)
+  else:
+    # if not post send form 
+    return render(request, 'signup1.html')
+
+
+
+
+#login post
+def login(request):
+  if request.method == 'POST':
+    username_form = request.POST['username']
+    password_form = request.POST['password']
+    # authenticate user
+    user = auth.authenticate(username=username_form, password=password_form)
+    if user is not None:
+      # login
+      auth.login(request, user)
+      #redirect
+      return redirect('artist_list')
+    else:
+      context = {'error':'Invalid Credentials'}
+      return render(request, 'login.html', context)
+  else:
+    return render(request, 'login.html')
+
+#logout
+def logout(request):
+    auth.logout(request)
+    return redirect('artist_list')
 
 # Create your views here.
--- burn/views.py
+++ burn/views.py
@@ -12,3 +12,32 @@ def dashboard(request, username):
   user_profile = User_profile.objects.get(user=user)
   meals = Meal.objects.filter(user=user)
   return render(request, 'dashboard.html', {'username': username, 'meals': meals, 'user_profile': user_profile})
+
+# #user metrics
+# def post_user_metrics(request):
+#     form = UserForm(request.POST)
+#     if form.is_valid():
+#         user = User(
+#             name=form.cleaned_data['name'],
+#             height=form.cleaned_data['height'],
+#             weight=form.cleaned_data['weight'],
+#             age=form.cleaned_data['age'],
+#             isMale=form.boolean_field(default=True),
+#             bmr=bmr(),
+#             targetweight=form.cleaned_data['targetweight'],
+#             goal=form.as_table['bulk', 'maintain_weight', 'lose_weight'],
+#         )
+
+#         User = form.save(commit = False)
+#         User.save()
+
+# #daily calorie intake calculation
+# def bmr():
+# # Harris-Benedict Equation
+#     if User.isMale:
+#         bmr = 66.47 + (6.24 * User.weight) + (12.7 * User.height) - (6.755 * User.age)
+#     else:
+#         bmr = 655.1 + (4.35 * User.weight) + (4.7 * User.height) - (4.7 * User.age)
+
+#     bmr = round(bmr)
+#     print(bmr)
