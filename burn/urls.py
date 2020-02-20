bd559f34af22f5e5dac822f8984ade90879133bd
billy-zeng <bzeng95@gmail.com>
2020-02-19 16:43:26 -0800

    adds gender to user profile and changes some integer fields to float fields

--- burn/urls.py
+++ burn/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.welcome, name='welcome'), #landing page 
-  path('dashboard/<username>/', views.dashboard, name='dashboard') # user dashboard
+  path('dashboard/<username>/', views.dashboard, name='dashboard'), # user dashboard
+  path('search/', views.search, name='search')  # search for food
 ]
--- burn/views.py
+++ burn/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import Meal, Food, User_profile
+import requests
 
 # Create your views here.
 
@@ -12,3 +13,6 @@ def dashboard(request, username):
   user_profile = User_profile.objects.get(user=user)
   meals = Meal.objects.filter(user=user)
   return render(request, 'dashboard.html', {'username': username, 'meals': meals, 'user_profile': user_profile})
+
+# def search(request):
+#   return render(request, 'search.html')
