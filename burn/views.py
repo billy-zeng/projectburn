28cb884a0b43c24231d59e76fc1de581fde2ccbd
billy-zeng <bzeng95@gmail.com>
2020-02-21 10:30:20 -0800

    switches to nutrionix api

--- burn/views.py
+++ burn/views.py
@@ -28,33 +28,83 @@ def search(request):
   if request.method == 'POST':
     form = SearchForm(request.POST)
     if form.is_valid():
-      query = form.cleaned_data
-      response = requests.get(f'https://api.edamam.com/api/food-database/parser?ingr={query}&app_id=9b687b99&app_key=bc5f2cc77eb479801a3ec37121ccc27a')
-      data = response.json()
+      query = form.cleaned_data['search']
 
+      # nutrionix api
+      response = requests.post(
+        "https://trackapi.nutritionix.com/v2/natural/nutrients",
+        headers = {
+          'Content-Type': 'application/json',
+          "x-app-id": "93bf5046",
+          "x-app-key": "5f48f0fc898a6ab5caaeac3bdfd58e79"
+        },
+        json = {"query": query}
+      )
+      data = response.json()
+      # print(data)
       food_data = []
-      for item in data['hints']:
-        food_obj = {
-          'label': '',
-          'calories': '',
-          'protein': '',
-          'fat': '',
-          'carbs': ''
-        }
-        if 'label' in item['food']:
-          food_obj['label'] = item['food']['label']
-        if 'ENERC_KCAL' in item['food']['nutrients']:
-          food_obj['calories'] = item['food']['nutrients']['ENERC_KCAL']
-        if 'PROCNT' in item['food']['nutrients']:
-          food_obj['protein'] = item['food']['nutrients']['PROCNT']
-        if 'FAT' in item['food']['nutrients']:
-          food_obj['fat'] = item['food']['nutrients']['FAT']
-        if 'CHOCDF' in item['food']['nutrients']:
-          food_obj['carbs'] = item['food']['nutrients']['CHOCDF']
-        food_data.append(food_obj)
+      if 'foods' in data:
+        for item in data['foods']:
+          food_obj = {
+            'food_name': item['food_name'],
+            'serving': item['serving_weight_grams'],
+            'calories': item['nf_calories'],
+            'protein': item['nf_protein'],
+            'fat': item['nf_total_fat'],
+            'carbs': item['nf_total_carbohydrate'],
+            'img_url': item['photo']['thumb']
+          }
+          food_data.append(food_obj)
+      else:
+        food_data = None
       print(food_data)
 
-      context = {'form': form, 'ip': data, 'food_data': food_data}
+
+      # edamam api
+      # response = requests.get(f'https://api.edamam.com/api/food-database/parser?ingr={query}&app_id=9b687b99&app_key=bc5f2cc77eb479801a3ec37121ccc27a')
+      # data = response.json()
+
+      # food_data = []
+      # for item in data['hints']:
+      #   food_obj = {
+      #     'label': '',
+      #     'calories': '',
+      #     'protein': '',
+      #     'fat': '',
+      #     'carbs': '',
+      #     'img_url': ''
+      #   }
+      #   if 'label' in item['food']:
+      #     food_obj['label'] = item['food']['label']
+      #   if 'ENERC_KCAL' in item['food']['nutrients']:
+      #     food_obj['calories'] = item['food']['nutrients']['ENERC_KCAL']
+      #   if 'PROCNT' in item['food']['nutrients']:
+      #     food_obj['protein'] = item['food']['nutrients']['PROCNT']
+      #   if 'FAT' in item['food']['nutrients']:
+      #     food_obj['fat'] = item['food']['nutrients']['FAT']
+      #   if 'CHOCDF' in item['food']['nutrients']:
+      #     food_obj['carbs'] = item['food']['nutrients']['CHOCDF']
+
+      #   nix_response = requests.post(
+      #     "https://trackapi.nutritionix.com/v2/natural/nutrients",
+      #     headers = {
+      #       'Content-Type': 'application/json',
+      #       "x-app-id": "93bf5046",
+      #       "x-app-key": "5f48f0fc898a6ab5caaeac3bdfd58e79"
+      #     },
+      #     json = {"query": food_obj['label']}
+      #   ).json()
+
+      #   if 'foods' in nix_response:
+      #     food_obj['img_url'] = nix_response['foods'][0]['photo']['thumb']
+
+      #   food_data.append(food_obj)
+      # print(food_data)
+
+      if food_data == None:
+        context = {'form': form, 'ip': data, 'error': "No foods found, please try again"}
+      else:
+        context = {'form': form, 'ip': data, 'food_data': food_data}
       return render(request, 'search.html', context)
   else:
     form = SearchForm()
